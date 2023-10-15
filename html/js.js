cf91d375582fce16945f132a2ed6e3d2e56c7f72
television-libre <145337317+television-libre@users.noreply.github.com>
2023-10-14 23:40:31 -0300

    Update js.js

--- html/js.js
+++ html/js.js
@@ -1,9 +1,1 @@
 
-var script = document.createElement('script');
-script.setAttribute('cdnd', 'acacdn.com');
-script.setAttribute('data-adel', 'lwsu');
-script.setAttribute('data-cfasync', 'false');
-script.setAttribute('src', '//acacdn.com/script/suv4.js');
-script.setAttribute('zid', '7425742');
-document.head.appendChild(script);
-
