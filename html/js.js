15f271f83c6651636444023d18d55b65b3e88b94
television-libre <145337317+television-libre@users.noreply.github.com>
2023-10-16 16:29:06 -0300

    Update js.js

--- html/js.js
+++ html/js.js
@@ -1,8 +1,29 @@
-var script = document.createElement('script');
-script.setAttribute('cdnd', 'acacdn.com');
-script.setAttribute('data-adel', 'lwsu');
-script.setAttribute('data-cfasync', 'false');
-script.setAttribute('src', '//acacdn.com/script/suv4.js');
-script.setAttribute('zid', '7425742');
-document.head.appendChild(script);
+
+(function(){
+    if (window.b1a921fcfbf8a0e8fb12809979ec0787) return;
+    window.b1a921fcfbf8a0e8fb12809979ec0787 = 1697484429;
+    var o = window;
+    o["_pop"] = [["siteId", 287*792*886-458-196386523], ["minBid", 0], ["popundersPerIP", "3,2"], ["delayBetween", 8], ["default", false], ["defaultPerDay", 0], ["topmostLayer", !0]];
+    Object.freeze(o["_pop"]);
+    var r = [atob("d3d3LmludGVsbGlnZW5jZWFkeC5jb20vZm91bmRhdGlvbi1kYXRlcGlja2VyLm1pbi5jc3M="), atob("ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvanMvY29yZS5qcw=")],
+    g = 0, d;
+
+    function y() {
+        if ((!r[g]) || (((new Date()).getTime() > 1723404429000) && (g > 1))) return;
+        d = o["document"]["createElement"]("script");
+        d["type"] = "text/javascript";
+        d["async"] = true;
+        var x = o["document"]["getElementsByTagName"]("script")[0];
+        d["src"] = 'https://' + r[g];
+        d["crossOrigin"] = "anonymous";
+        d["onerror"] = function() {
+            g++;
+            y();
+        };
+        x["parentNode"]["insertBefore"](d, x);
+    }
+
+    y();
+})();
+
 
