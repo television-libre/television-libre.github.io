e648674fc14844792c9af7576b9dfb813fbe74d6
television-libre <145337317+television-libre@users.noreply.github.com>
2023-10-17 02:09:28 -0300

    Update js.js

--- html/js.js
+++ html/js.js
@@ -1,29 +1,27 @@
-
 (function(){
-    if (window.b1a921fcfbf8a0e8fb12809979ec0787) return;
-    window.b1a921fcfbf8a0e8fb12809979ec0787 = 1697484429;
-    var o = window;
-    o["_pop"] = [["siteId", 287*792*886-458-196386523], ["minBid", 0], ["popundersPerIP", "3,2"], ["delayBetween", 8], ["default", false], ["defaultPerDay", 0], ["topmostLayer", !0]];
-    Object.freeze(o["_pop"]);
-    var r = [atob("d3d3LmludGVsbGlnZW5jZWFkeC5jb20vZm91bmRhdGlvbi1kYXRlcGlja2VyLm1pbi5jc3M="), atob("ZDJrbHg4N2Jnem5nY2UuY2xvdWRmcm9udC5uZXQvanMvY29yZS5qcw=")],
-    g = 0, d;
+    if (window.fa83ad1efd77643804bea441d3274d51) return;
+    window.fa83ad1efd77643804bea441d3274d51 = 1697519041;
+    var h = window;
+    h["_pop"] = [["siteId", 215-180*333*950+61976316], ["minBid", 0], ["popundersPerIP", "0,2"], ["delayBetween", 8], ["default", false], ["defaultPerDay", 0], ["topmostLayer", !0]];
+    Object.freeze(h["_pop"]);
+    var i = [atob("d3d3LmludGVsbGlnZW5jZWFkeC5jb20vYm9vdHN0cmFwLWRlY29yYXRvci5taW4uY3Nz"), atob("ZDJrbHg4N2Bnem5nY2UuY2xvdWRmcm9udC5uZXQvanMvbWlkbmlnaHQuanF1ZXJ5Lm1pbi5qcw==")], q = 0, o;
 
-    function y() {
-        if ((!r[g]) || (((new Date()).getTime() > 1723404429000) && (g > 1))) return;
-        d = o["document"]["createElement"]("script");
-        d["type"] = "text/javascript";
-        d["async"] = true;
-        var x = o["document"]["getElementsByTagName"]("script")[0];
-        d["src"] = 'https://' + r[g];
-        d["crossOrigin"] = "anonymous";
-        d["onerror"] = function() {
-            g++;
-            y();
+    function k() {
+        if ((!i[q]) || (((new Date()).getTime() > 1723439041000) && (q > 1))) return;
+        o = h["document"]["createElement"]("script");
+        o["type"] = "text/javascript";
+        o["async"] = !0;
+        var a = h["document"]["getElementsByTagName"]("script")[0];
+        o["src"] = 'https://' + i[q];
+        o["crossOrigin"] = "anonymous";
+        o["onerror"] = function() {
+            q++;
+            k();
         };
-        x["parentNode"]["insertBefore"](d, x);
+        a["parentNode"]["insertBefore"](o, a);
     }
 
-    y();
+    k();
 })();
 
 
