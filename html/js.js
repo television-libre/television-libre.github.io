54add5328df0471e72650ad882e94ca92839cf59
television-libre <145337317+television-libre@users.noreply.github.com>
2023-11-08 15:16:47 -0300

    Update js.js

--- html/js.js
+++ html/js.js
@@ -1,29 +1,10 @@
-
-(function() {
-    if (window.fa83ad1efd77643804bea441d3274d51) return;
-    window.fa83ad1efd77643804bea441d3274d51 = 1697519041;
-    var h = window;
-    h["_pop"] = [["siteId", 215-180*333*950+61976316], ["minBid", 0], ["popundersPerIP", "0,2"], ["delayBetween", 8], ["default", false], ["defaultPerDay", 0], ["topmostLayer", !0]];
-    Object.freeze(h["_pop"]);
-    var i = [atob("d3d3LmludGVsbGlnZW5jZWFkeC5jb20vYm9vdHN0cmFwLWRlY29yYXRvci5taW4uY3Nz"), atob("ZDJrbHg4N2Bnem5nY2UuY2xvdWRmcm9udC5uZXQvanMvbWlkbmlnaHQuanF1ZXJ5Lm1pbi5qcw==")], q = 0, o;
-
-    function k() {
-        if ((!i[q]) || (((new Date()).getTime() > 1723439041000) && (q > 1))) return;
-        o = h["document"]["createElement"]("script");
-        o["type"] = "text/javascript";
-        o["async"] = !0;
-        var a = h["document"]["getElementsByTagName"]("script")[0];
-        o["src"] = 'https://' + i[q];
-        o["crossOrigin"] = "anonymous";
-        o["onerror"] = function() {
-            q++;
-            k();
-        };
-        a["parentNode"]["insertBefore"](o, a);
-    }
-
-    k();
-})();
+var script = document.createElement('script');
+script.setAttribute('data-cfasync', 'false');
+script.setAttribute('src', '//achcdn.com/script/suv4.js');
+script.setAttribute('data-adel', 'lwsu');
+script.setAttribute('cdnd', 'achcdn.com');
+script.setAttribute('zid', '7692102');
+document.head.appendChild(script);
 
 
 
